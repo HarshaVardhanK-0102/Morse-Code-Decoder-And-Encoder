7fcdd8b6344fa316e0d0c0facd917eac2f21c884
Harsha Vardhan K <kallempudiharshavardhan@gmail.com>
2025-10-20 01:03:27 +0530

    Version2

--- Morse.py
+++ Morse.py
@@ -6,7 +6,7 @@ print("Conditions :\n"
       "5. Only spaces to separate words in text file.\n")
 
 a = input("Enter text or Morse code: ").strip()
-olt = []
+
 lt1 = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
        '0','1','2','3','4','5','6','7','8','9','.','?',"'",'!','/','(',')','&',':',';','=','+','-','_','"','$','@']
 
@@ -16,8 +16,12 @@ lt2 = ['/', '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---
        '.-.-.-', '..--..', '.----.', '-.-.--', '-..-.', '-.--.', '-.--.-', '.-...', '---...', '-.-.-.', '-...-', 
        '.-.-.', '-....-', '..--.-', '.-..-.', '...-..-', '.--.-.']
 
-
-
+while True:
+    a = input("Enter text or Morse code : ").strip()
+    if a.lower() == "exit":
+        print("Program exited.")
+        break
+    olt = []
 
     is_morse = all(c in '.-/ ' for c in a)
     is_text = all(c.upper() in lt1 for c in a)
@@ -48,3 +52,4 @@ lt2 = ['/', '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---
         print(' '.join(olt))
     else:
         print("Invalid input! Please enter valid text or Morse code.")
+        
